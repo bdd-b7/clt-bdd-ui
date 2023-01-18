68c5dfdecb75fa0b49e5c7ca0a8a3755c2ba69e7
deepathalaivan <deepathalaivan@gmail.com>
2023-01-17 19:25:31 -0600

    Updates

--- step-definitions/jootza-steps.js
+++ step-definitions/jootza-steps.js
@@ -1,9 +1,6 @@
 module.exports = function () {
 
     //Given I am on the "Jootza" portal
-
-   
-
     this.Given('I am on the Jootza portal', function () {
         return helpers.loadPage(page.jootza.url);
     });
@@ -50,6 +47,7 @@ module.exports = function () {
         page.commonObjects.inputElement('password', password)
         page.commonObjects.clickElement('LoginBtn')
         await driver.sleep(2000); 
+        
         return page.commonObjects.elementExists('WelcomeMessage')
     })
 
